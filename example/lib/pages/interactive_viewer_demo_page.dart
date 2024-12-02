b397a5bd998f47797f2f936b841e3d5b1fd80ded
AoEiuV020 <aoeiuv020@gmail.com>
2024-12-02 16:01:37 +0800

    example实现InteractiveViewer双击重置缩放状态

--- example/lib/pages/interactive_viewer_demo_page.dart
+++ example/lib/pages/interactive_viewer_demo_page.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 
 import '../example_widget.dart';
 
-class InteractiveViewerDemoPage extends StatelessWidget {
+class InteractiveViewerDemoPage extends StatefulWidget {
   const InteractiveViewerDemoPage({super.key});
+
+  @override
+  State<InteractiveViewerDemoPage> createState() =>
+      _InteractiveViewerDemoPageState();
+}
+
+class _InteractiveViewerDemoPageState extends State<InteractiveViewerDemoPage> {
+  final TransformationController _transformationController =
+      TransformationController();
+
+  void _handleDoubleTap() {
+    _transformationController.value = Matrix4.identity();
+  }
+
+  @override
+  void dispose() {
+    _transformationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +31,16 @@ class InteractiveViewerDemoPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('InteractiveViewer演示'),
       ),
-      body: InteractiveViewer(
-        clipBehavior: Clip.none,
-        boundaryMargin: const EdgeInsets.all(20.0),
-        minScale: 0.01,
-        maxScale: 40.0,
-        trackpadScrollCausesScale: true,
-        child: const ExampleWidget(),
+      body: GestureDetector(
+        onDoubleTap: _handleDoubleTap,
+        child: InteractiveViewer(
+          transformationController: _transformationController,
+          clipBehavior: Clip.none,
+          minScale: 0.01,
+          maxScale: 40.0,
+          trackpadScrollCausesScale: true,
+          child: const ExampleWidget(),
+        ),
       ),
     );
   }
